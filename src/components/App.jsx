422d0503b6c08736a19bef593dde8a995301bf93
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-08-30 18:34:29 +0300

    1 Task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,18 @@
 
 import userData from '../assets/userData.json';
-import s from "./Profile/Profile.module.css";
-console.log(userData);
+import Profile from './Profile/Profile';
 
-function App() {
-    return (
-        <div className={s.card}>
-      <div className={s.img}>
-        <img
-          src={userData.avatar}
-          alt="User avatar"
-        />
-        <p>{userData.username}</p>
-        <p>@{userData.tag}</p>
-        <p>{userData.location}</p>
-      </div>
-
-  <ul className={s.list}>
-    <li>
-      <span>Followers: </span>
-      <span>{userData.stats.followers}</span>
-    </li>
-    <li>
-      <span>Views: </span>
-      <span>{userData.stats.views}</span>
-    </li>
-    <li>
-      <span>Likes: </span>
-      <span>{userData.stats.likes}</span>
-    </li>
-  </ul>
-</div>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
 export default App
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+import s from './Profile.module.css';
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={s.card}>
+      <div className={s.img}>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul className={s.list}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
