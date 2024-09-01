a414637fbbe592521cbeed4622cc80716179581a
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-09-01 10:51:06 +0300

    repair

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,19 @@
-import FriendListItem from './FriendListItem';
-import s from './FriendList.module.css';
+import FriendListItem from "../FriendListItem/FriendListItem";
+import s from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
-    return (
-        <ul className={s.friendList}>
-            {friends.map(friend => (
-                <FriendListItem
-                    key={friend.id}
-                    avatar={friend.avatar}
-                    name={friend.name}
-                    isOnline={friend.isOnline}
-                />
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={s.friendList}>
+      {friends.map((friend) => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default FriendList;
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import s from "../FriendList/FriendList.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={s.item}>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+      <span className={isOnline ? s.online : s.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </span>
+    </li>
+  );
+};
+
+export default FriendListItem;
